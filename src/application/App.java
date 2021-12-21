5847014d28356f26b762ffc004505fad8d2aa0bd
earl-stephens <stephensearl@hotmail.com>
2021-12-20 19:35:52 -0700

    Exploring different uses of not operator

--- src/application/App.java
+++ src/application/App.java
@@ -3,8 +3,28 @@ package application;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int value1 = 4;
+		int value2 = 3;
+
+		if (value1 != value2) {
+			System.out.println("Values are not equal.");
+		}
+		// != is a binary operator that outputs a boolean
+		
+		String fruit1 = "orange";
+		String fruit2 = "apple";
+		
+		if(!fruit1.equals(fruit2)) {
+			System.out.println("Fruits are not the same.");
+		}
+		// can't use != for string comparison
+		
+		// can also use this format
+		// but it's confusing
+		if(fruit1.equals(fruit2) != true)  {
+			System.out.println("Fruits are not the same.");
+		}
 	}
 
 }
